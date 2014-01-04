3e998abf39b3145dbbc3bcc9f428ae5bc2a0a0a0
Xing Wu <xingwu.cs@gmail.com>
2014-01-04 16:56:08 -0500

    health data reading and analysis

---
+++ subprojects/s4-ddm/src/main/java/org/apache/s4/ddm/HealthStats.java
@@ -0,0 +1,302 @@
+package org.apache.s4.ddm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.beust.jcommander.internal.Maps;
+
+public class HealthStats {
+    static Logger logger = LoggerFactory.getLogger(HealthStats.class);
+
+    private final static String statsUrl = "http://node-1.lan20.idsp.emulab.net:8080/render/?target=S4-cluster*.*.*-pe-processing-time.mean&target=S4-cluster*.*.*-pe-processing-time.m1_rate&format=csv&from=-5minutes";
+
+    public class PeLoadStat {
+        PeLoadStat() {
+            eventsCount = 0;
+            procTime = 0.0;
+            eventsRound = 0;
+            procRound = 0;
+        }
+
+        public double getLoad() {
+            // logger.debug("{} {} load: " + eventsCount * procTime, eventsCount, procTime);
+            return eventsCount * procTime;
+        }
+
+        double eventsCount;
+        double procTime;
+        int eventsRound;
+        int procRound;
+    }
+
+    public class ClusterStats implements Comparable<ClusterStats> {
+        public String name;
+        public Double load;
+
+        public ClusterStats(String n, double l) {
+            name = n;
+            load = l;
+        }
+
+        @Override
+        public int compareTo(ClusterStats arg0) {
+            return this.load > arg0.load ? 1 : this.load < arg0.load ? -1 : 0;
+        }
+    }
+
+    // map<cluster, map<stream, s>>
+    private Map<String, Map<String, List<PeLoadStat>>> mapStats = Maps.newHashMap();
+    List<ClusterStats> orderedClusters = new ArrayList<ClusterStats>();
+
+    public HealthStats() {
+    }
+
+    private List<Double> getLoadSeriesOfStream(String clusterName, String streamName) {
+        Map<String, List<PeLoadStat>> streams = mapStats.get(clusterName);
+        if (streams == null) {
+            logger.debug("{} doesn't exists!", clusterName);
+            return null;
+        }
+
+        List<PeLoadStat> stats = streams.get(streamName);
+        if (stats == null) {
+            logger.debug("{}-{} doesn't exists!", clusterName, streamName);
+            return null;
+        }
+
+        List<Double> list = new ArrayList<Double>();
+        for (int i = 0; i < stats.size(); i++) {
+            list.add(new Double(stats.get(i).getLoad()));
+        }
+        return list;
+    }
+
+    private List<Double> getLoadSeriesOfClusterExceptStream(String clusterName, String streamName) {
+        Map<String, List<PeLoadStat>> streams = mapStats.get(clusterName);
+        if (streams == null) {
+            logger.debug("{} doesn't exists!", clusterName);
+            return null;
+        } else {
+            List<Double> list = new ArrayList<Double>();
+            for (Map.Entry<String, List<PeLoadStat>> streamEntrys : streams.entrySet()) {
+                if (streamEntrys.getKey().equals(streamName))
+                    continue;
+                for (int i = 0; i < streamEntrys.getValue().size(); i++) {
+                    Double d;
+                    if (i < list.size()) {
+                        d = list.get(i);
+                    } else {
+                        d = new Double(0.0);
+                        list.add(d);
+                    }
+                    d += streamEntrys.getValue().get(i).getLoad();
+                    list.set(i, d);
+                }
+            }
+            return list;
+        }
+    }
+
+    private List<Double> getLoadSeriesOfCluster(String clusterName) {
+        Map<String, List<PeLoadStat>> streams = mapStats.get(clusterName);
+        if (streams == null) {
+            logger.debug("{} doesn't exists!", clusterName);
+            return null;
+        } else {
+            List<Double> list = new ArrayList<Double>();
+            for (List<PeLoadStat> streamStats : streams.values()) {
+                for (int i = 0; i < streamStats.size(); i++) {
+                    Double d;
+                    if (i < list.size()) {
+                        d = list.get(i);
+                    } else {
+                        d = new Double(0.0);
+                        list.add(d);
+                    }
+                    d += streamStats.get(i).getLoad();
+                    list.set(i, d);
+                }
+            }
+            return list;
+        }
+    }
+
+    public double averageLoad(List<Double> list) {
+        if (list == null) {
+            logger.debug("list is null");
+            return 0.0;
+        }
+        double a = 0.0;
+        for (Double d : list) {
+            // logger.debug("v: " + d);
+            a += d;
+        }
+        if (list.size() == 0) {
+            logger.debug("list is empty");
+            return 0.0;
+        } else {
+            return a / list.size();
+        }
+    }
+
+    public double var(List<Double> list) {
+        if (list.size() == 0)
+            return 0;
+        double sums2 = 0.0;
+        double sums = 0.0;
+        for (Double d : list) {
+            sums2 += Math.pow(d, 2.0);
+            sums += d;
+        }
+        return sums2 / list.size() - Math.pow(sums / list.size(), 2.0);
+    }
+
+    public double cov(List<Double> list1, List<Double> list2) {
+        if (list1.size() == 0 || list2.size() == 0)
+            return 0.0;
+        double sums1s2 = 0.0;
+        for (int i = 0; i < list1.size() && i < list2.size(); i++) {
+            sums1s2 += list1.get(i) * list2.get(i);
+        }
+        return sums1s2 / list1.size() - averageLoad(list1) * averageLoad(list2);
+    }
+
+    public double correlation(List<Double> list1, List<Double> list2) {
+        return cov(list1, list2) / (Math.sqrt(var(list1)) * Math.sqrt(var(list2)));
+    }
+
+    public void orderClusters() {
+        for (String clusterName : mapStats.keySet()) {
+            orderedClusters.add(new ClusterStats(clusterName, averageLoad(getLoadSeriesOfCluster(clusterName))));
+        }
+        Collections.sort(orderedClusters);
+    }
+
+    public void checkClusterPair(String cluster1, String cluster2) {
+        double corr = correlation(getLoadSeriesOfCluster(cluster1), getLoadSeriesOfCluster(cluster2));
+        logger.debug("Correlation of {} and {} is " + corr, cluster1, cluster2);
+    }
+
+    public void analyze() {
+        for (int i = 0, j = orderedClusters.size() - 1; i < j; i++, j--) {
+            checkClusterPair(orderedClusters.get(i).name, orderedClusters.get(j).name);
+        }
+    }
+
+    public void printOrderedClusters() throws IOException {
+        for (ClusterStats s : orderedClusters) {
+            System.out.println(s.name + " - " + s.load);
+        }
+    }
+
+    public void initData() throws IOException {
+        URL url = new URL(statsUrl);
+        URLConnection connection = url.openConnection();
+        InputStreamReader stream = new InputStreamReader(connection.getInputStream());
+        BufferedReader in = new BufferedReader(stream);
+
+        String line;
+        String clusterName = "";
+        String streamName = "";
+        List<PeLoadStat> lastList = null;
+        int i = 0;
+        boolean bAllZero = true;
+        List<String> content = new ArrayList<String>();
+        while ((line = in.readLine()) != null) {
+            content.add(line);
+        }
+        in.close();
+        logger.debug("file size: " + content.size() + " lines");
+        for (String dataLine : content) {
+            // dataLine =
+            // "S4-cluster3-0.TopicSeen.TopicCountAndReportPE-pe-processing-time.count,2014-01-03 15:52:00,39368407.0";
+            // logger.debug(dataLine);
+            Pattern pat = Pattern.compile("S4\\-([^\\-]+)\\-\\d+\\.([^\\.]+)\\.[^.]+\\.([^,]+),[^,]+,(.*)$");
+            Matcher m = pat.matcher(dataLine);
+            if (!m.matches()) {
+                mapStats.clear();
+                return;
+            }
+            if (!clusterName.equals(m.group(1)) || !streamName.equals(m.group(2))) {
+                i = 0;
+                if (bAllZero && lastList != null) {
+                    mapStats.get(clusterName).remove(streamName);
+                    logger.debug("delete all zero list. {} - {}", clusterName, streamName);
+                }
+                clusterName = m.group(1);
+                streamName = m.group(2);
+                logger.debug("newlist. {} - {}", clusterName, streamName);
+                bAllZero = true;
+                Map<String, List<PeLoadStat>> tmpStreamMap = mapStats.get(clusterName);
+                if (tmpStreamMap == null) {
+                    tmpStreamMap = Maps.newHashMap();
+                    mapStats.put(clusterName, tmpStreamMap);
+                }
+                lastList = tmpStreamMap.get(streamName);
+                if (lastList == null) {
+                    lastList = new ArrayList<PeLoadStat>();
+                    tmpStreamMap.put(streamName, lastList);
+                }
+            }
+
+            PeLoadStat tmp;
+            if (i < lastList.size()) {
+                tmp = lastList.get(i);
+            } else {
+                tmp = new PeLoadStat();
+                lastList.add(tmp);
+            }
+            i++;
+            if (m.group(3).equals("mean")) {
+                try {
+                    tmp.procTime = (tmp.procTime * tmp.procRound + Double.parseDouble(m.group(4)))
+                            / (tmp.procRound + 1);
+                    tmp.procRound++;
+                } catch (NumberFormatException e) {
+                    tmp.procTime = 0.0;
+                }
+                if (tmp.procTime != 0.0) {
+                    bAllZero = false;
+                }
+            } else {
+                try {
+                    tmp.eventsCount = (tmp.eventsCount * tmp.eventsRound + Double.parseDouble(m.group(4)) * 6)
+                            / (tmp.eventsRound + 1);
+                    tmp.eventsRound++;
+                } catch (NumberFormatException e) {
+                    tmp.eventsCount = 0;
+                }
+                if (tmp.eventsCount != 0) {
+                    bAllZero = false;
+                }
+            }
+        }
+
+    }
+
+    public void printData() throws IOException {
+        for (Map.Entry<String, Map<String, List<PeLoadStat>>> entry : mapStats.entrySet()) {
+            System.out.println("=" + entry.getKey() + "=");
+            for (Map.Entry<String, List<PeLoadStat>> listEntry : entry.getValue().entrySet()) {
+                System.out.println("--" + listEntry.getKey());
+                for (PeLoadStat stat : listEntry.getValue()) {
+                    System.out.print("(" + stat.procTime + "," + stat.eventsCount + ",");
+                    System.out.print(stat.procRound + "," + stat.eventsRound + "), ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
